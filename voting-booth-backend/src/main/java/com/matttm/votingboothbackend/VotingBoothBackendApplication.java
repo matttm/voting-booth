e8a548beedb7ef780b202c62fd6a68bacaa50aab
matttm <matttmaloney@gmail.com>
2020-07-14 18:18:33 -0400

    corrected spring initializr'd project

--- voting-booth-backend/src/main/java/com/matttm/votingboothbackend/VotingBoothBackendApplication.java
+++ voting-booth-backend/src/main/java/com/matttm/votingboothbackend/VotingBoothBackendApplication.java
@@ -1,13 +1,13 @@
-package com.matttm.bugtracker.demo;
+package com.matttm.votingboothbackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class VotingBoothBackendApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(VotingBoothBackendApplication.class, args);
 	}
 
 }
--- voting-booth-backend/src/test/java/com/matttm/votingboothbackend/VotingBoothBackendApplicationTests.java
+++ voting-booth-backend/src/test/java/com/matttm/votingboothbackend/VotingBoothBackendApplicationTests.java
@@ -1,10 +1,10 @@
-package com.matttm.bugtracker.demo;
+package com.matttm.votingboothbackend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class VotingBoothBackendApplicationTests {
 
 	@Test
 	void contextLoads() {
