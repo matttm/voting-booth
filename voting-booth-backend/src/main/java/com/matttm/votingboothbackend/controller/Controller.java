526f7f6b2cf89b859c633b9a2ce55bf0692fc5e7
matttm <matttmaloney@gmail.com>
2020-07-18 09:37:52 -0400

    moved actual entry point class and added more to controller skeleton

--- voting-booth-backend/src/main/java/com/matttm/votingboothbackend/controller/Controller.java
+++ voting-booth-backend/src/main/java/com/matttm/votingboothbackend/controller/Controller.java
@@ -1,6 +1,9 @@
 package com.matttm.votingboothbackend.controller;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -11,10 +14,19 @@ import org.springframework.web.bind.annotation.RestController;
  * Language:  Java 1.8
  */
 @RestController
+@RequestMapping("/api")
 public class Controller {
 
-    @GetMapping("/api/test")
+    @GetMapping("/test")
     public String testServer() {
         return "Testing...";
+    }
+
+    @PostMapping("/register")
+    public void register() {}
+    
+    @GetMapping("/validate")
+    public ResponseEntity<Boolean> validate() {
+        return null;
     }
 }
