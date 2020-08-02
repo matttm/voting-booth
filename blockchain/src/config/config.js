b52b55f781b88f3da83e2975f0406ee78bf3214d
matttm <matttmaloney@gmail.com>
2020-08-02 10:06:06 -0400

    added configuration object

---
+++ blockchain/src/config/config.js
@@ -0,0 +1,12 @@
+/**
+ * Everything used in configuration of the blockchain
+ * @type {{}} configuration object
+ */
+const config = {};
+
+config.addresss = 'https://localhost:3000';
+config.peers = [
+    'https://localhost:3000'
+];
+
+module.exports = config;
