36b305ed19e6def5edbfd0edc12c05cf72415c50
matttm <matttmaloney@gmail.com>
2020-08-02 13:48:38 -0400

    stubbed out blockchain server

--- blockchain-server/src/server/routers/api.js
+++ blockchain-server/src/server/routers/api.js
@@ -5,4 +5,20 @@ router.get('/', function(req, res, next) {
   res.send('Specify a resource in the URL');
 });
 
+router.get('/blocks', (req, res) => {
+
+});
+
+router.post('/blocks', (req, res) => {
+
+});
+
+router.get('/peers', (req, res) => {
+
+});
+
+router.post('peers', (req, res) => {
+
+});
+
 module.exports = router;
