afbadf29d54089e3802f659ff9a6a873e04c6f26
matttm <matttmaloney@gmail.com>
2020-08-01 22:08:11 -0400

    added chain validation

--- blockchain/src/utilities.js
+++ blockchain/src/utilities.js
@@ -5,7 +5,7 @@
  *
  * @returns {boolean} true iff the block is valid
  */
-function isBlockValid(newBlock, latestBlock) {
+function isValidBlock(newBlock, latestBlock) {
     const hash = newBlock.hash;
 
     if (hash === latestBlock.hash) {
@@ -14,6 +14,25 @@ function isBlockValid(newBlock, latestBlock) {
     } else if (newBlock.isHashValid(hash)) {
         console.log(`Block ${newBlock.timestamp} rejected: new hash does match block's difficulty`);
         return false;
+    }
+    return true;
+}
+
+/**
+ * Determine whether an entire chain is valid
+ * @param blockchain a blockchain object to be validated
+ *
+ * @returns {boolean} true iff every block in the chain is valid
+ */
+function isValidChain(blockchain) {
+    const chain = blockchain.chain;
+
+    // TODO: validate genesis
+    // this loop verifies all blocks after genesis block
+    for (let i = 1; i < chain.length; i++) {
+        if (!isValidBlock(chain[i], chain[i-1])) {
+            return false;
+        }
     }
     return true;
 }
