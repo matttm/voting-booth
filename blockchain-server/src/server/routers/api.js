7834c297d8a95a0c44d9f85bbffee2f715241189
matttm <matttmaloney@gmail.com>
2020-08-09 09:56:08 -0400

    filled in get/post requests for blocks url in api route

--- blockchain-server/src/server/routers/api.js
+++ blockchain-server/src/server/routers/api.js
@@ -6,11 +6,25 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/blocks', (req, res) => {
-
+  // TODO: is there any checks to be done here?
+  res.status(200).json({
+    success: true,
+    chain: []  // TODO: give real blockchain
+  });
 });
 
 router.post('/blocks', (req, res) => {
-
+  const block = req.body?.block;
+  if (!block) {
+    res.status(400).json({
+      success: false,
+      message: "Request did not contain a block"
+    });
+  }
+  res.status(200).json({
+    success: true,
+    message: "Block added"
+  });
 });
 
 router.get('/peers', (req, res) => {
