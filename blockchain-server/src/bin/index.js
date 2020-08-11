30a58f4fd55e5825d6c5e0c706fa6c241f710fd3
matttm <matttmaloney@gmail.com>
2020-08-11 17:59:40 -0400

    stubbed out needed command line args

--- blockchain-server/src/bin/index.js
+++ blockchain-server/src/bin/index.js
@@ -14,8 +14,12 @@ const debug = _debug('blockchain:bin');
 /**
  * Get port from environment and store in Express.
  */
+// TODO: get args appropriately
 const httpPort = process.env.HTTP_PORT || config.httpPort || '3001';
 const p2pPort  = process.env.WS_PORT || config.wsPort || '5001';
+const httpAddress = null;
+const p2pAddress = null;
+const peers = null;
 
 /**
  * Create blockchain and give to the request handler (app)
@@ -24,7 +28,7 @@ const p2pPort  = process.env.WS_PORT || config.wsPort || '5001';
 debug("Instantiating blockchain and servers");
 const blockchain = new Blockchain();
 const httpServer = new HttpServer(blockchain, httpPort, httpAddress);
-const p2pServer = new P2pServer(blockchain, p2pPort, p3pAddress, peers);
+const p2pServer = new P2pServer(blockchain, p2pPort, p2pAddress, peers);
 
 /**
  * Start the api and the peer-to-peer connection
