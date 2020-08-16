2c86517ed8d3469251b432f30e2e54da388b96e5
matttm <matttmaloney@gmail.com>
2020-08-16 12:54:26 -0400

    added to http server test

--- blockchain-server/tests/http-server.spec.js
+++ blockchain-server/tests/http-server.spec.js
@@ -1,4 +1,28 @@
+import request from 'supertest';
+import {HttpServer} from "../src/servers";
+import {Blockchain} from "../src/blockchain";
 
 describe('http-server unit test', () => {
     // TODO: fill in test
+    let server;
+
+    beforeEach(() => {
+        server = new HttpServer(
+            new Blockchain()
+            , 'localhost',
+            '3001'
+        ).server;
+    });
+
+    test('should get blocks', (done) => {
+        request(server)
+            .get('/api/blocks')
+            .expect('Content-Type', /json/)
+            .expect(200)
+            .end((err, res) => {
+                if (err) throw err;
+                expect(res).toBeTruthy();
+                done();
+            });
+    });
 });
