31928ad5755fdb3aeb6542c92ca635bd73a2990f
matttm <matttmaloney@gmail.com>
2020-08-19 06:01:16 -0400

    fixed component rendering twice

--- voting-booth-frontend/src/app/app-routing.module.ts
+++ voting-booth-frontend/src/app/app-routing.module.ts
@@ -1,11 +1,8 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import {RouterModule, Routes} from '@angular/router';
 import {LoginComponent} from './components/login/login.component';
-import {AppComponent} from './components/app/app.component';
 
 const routes: Routes = [
-  { path: '', component: AppComponent },
   { path: 'login', component: LoginComponent }
 ];
 
