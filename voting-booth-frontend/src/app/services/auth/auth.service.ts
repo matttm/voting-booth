3eb1436ea6414890a7b75bfdbf24c4530b8b6c57
matttm <matttmaloney@gmail.com>
2020-08-29 11:26:22 -0400

    added comments

--- voting-booth-frontend/src/app/services/auth/auth.service.ts
+++ voting-booth-frontend/src/app/services/auth/auth.service.ts
@@ -26,17 +26,27 @@ export class AuthService {
       );
   }
 
+  /**
+   * Removes saved tokens used for authentication
+   */
   logout() {
     localStorage.removeItem('id_token');
     localStorage.removeItem('expires_at');
   }
 
+  /**
+   * Saves authentication token
+   * @param authToken json token received from the authentication server
+   */
   private setSession(authToken: JsonWebToken) {
     const expiresAt  = moment().add(authToken.expiresIn);
     localStorage.setItem('id_token', authToken.tokenId);
     localStorage.setItem('expires_at', JSON.stringify(expiresAt));
   }
 
+  /**
+   * Returns the time that the authentication token expires
+   */
   getExpiration() {
     const expiration = localStorage.getItem('expires_at');
     const expiresAt = JSON.parse(expiration);
