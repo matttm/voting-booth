b2e10bbaa934aedb9d2c12ff7632b0722f5e401c
matttm <matttmaloney@gmail.com>
2020-09-07 16:48:26 -0400

    made test for loading component

--- voting-booth-frontend/src/app/components/submission/submission.component.spec.ts
+++ voting-booth-frontend/src/app/components/submission/submission.component.spec.ts
@@ -1,6 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { SubmissionComponent } from './submission.component';
+import {MatProgressSpinnerModule} from '@angular/material';
 
 describe('SubmissionComponent', () => {
   let component: SubmissionComponent;
@@ -8,6 +9,7 @@ describe('SubmissionComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [ MatProgressSpinnerModule ],
       declarations: [ SubmissionComponent ]
     })
     .compileComponents();
@@ -21,5 +23,25 @@ describe('SubmissionComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should show ng-content when condition is false', () => {
+    const el: HTMLElement = fixture.debugElement.nativeElement;
+    fixture.componentInstance.conditionVar = false;
+    fixture.detectChanges();
+    const spinner = el.querySelector('#loading');
+    expect(spinner).toBeFalsy();
+    const children = el.querySelector('#non-loading');
+    expect(children).toBeTruthy();
+  });
+
+  it('should show spinner when condition is true', () => {
+    const el: HTMLElement = fixture.debugElement.nativeElement;
+    fixture.componentInstance.conditionVar = true;
+    fixture.detectChanges();
+    const spinner = el.querySelector('#loading');
+    expect(spinner).toBeTruthy();
+    const children = el.querySelector('#non-loading');
+    expect(children).toBeFalsy();
   });
 });
