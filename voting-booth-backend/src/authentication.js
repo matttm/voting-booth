ccbc91979a0ed1625c908e2e5fa55b2a2f2feb56
matttm <matttmaloney@gmail.com>
2020-09-11 21:05:35 -0400

    added jwt generation

---
+++ voting-booth-backend/src/authentication.js
@@ -0,0 +1,10 @@
+/**
+ * Determines whether provided information is of an authentic user
+ *
+ * @param fname a first name
+ * @param lname a last name
+ * @param ssn a social security number
+ * @param zip a zip code
+ */
+export function isValid(fname, lname, ssn, zip) {
+}
--- voting-booth-backend/src/routes/api.js
+++ voting-booth-backend/src/routes/api.js
@@ -1,9 +1,30 @@
+import fs from 'fs';
+import {isValid} from "../authentication";
 var express = require('express');
 var router = express.Router();
 
+const RSA_PRIVATE_KEY = fs.readFileSync('./private.key');
+
 /* GET users listing. */
 router.get('/vote', (req, res) => {
+    // get all info needed to authenticate
+    const { fname, lname, ssn, zip } = req.body;
 
+    if (isValid(fname, lname, ssn, zip)) {
+        const userId = findUserIdForEmail(email);
+        const jwtBearerToken = jwt.sign({}, RSA_PRIVATE_KEY, {
+            algorithm: 'RS256',
+            expiresIn: 120,
+            subject: userId
+        });
+        // TODO: conform key names
+        res.status(200).json({
+            tokenId: jwtBearerToken,
+            expiresIn: null
+        })
+    } else {
+        res.sendStatus(401);
+    }
 });
 
 router.post('/voted', (req, res) => {
