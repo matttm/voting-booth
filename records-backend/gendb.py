5cf77200372311524cffb26d8d349f802e1422dd
matttm <matttmaloney@gmail.com>
2020-09-22 16:05:44 -0400

    working on db gen

--- records-backend/gendb.py
+++ records-backend/gendb.py
@@ -10,12 +10,36 @@ def create_connection(db_file):
     try:
         conn = sqlite3.connect(db_file)
         print(sqlite3.version)
+        return conn
     except Error as e:
         print(e)
-    finally:
-        if conn:
-            conn.close()
+        exit(-1)
+
+def build_database(conn):
+    """
+    Build 'persons' table in db
+    :param ocnn: a sqlite3 connection that is open
+    :return:
+    """
+    cur = conn.cursor()
+    # TODO: change table name to 'persons'
+    table = """
+    CREATE TABLE person (
+        id integer PRIMARY KEY,
+        ssn text not null,
+        Fname text NOT NULL,
+        lname text NOT NULL,
+        zip text NOT NULL
+    )"""
+    cur.execute(table)
+    insert_sql = "INSERT INTO person (ssn, fname, lname, zip) VALUES (?, ?, ?, ?)"
+    cur = conn.cursor()
+    cur.execute(insert_sql, ('1892029', 'afname', 'alname', '19029'))
+    conn.commit()
 
 
 if __name__ == '__main__':
-    create_connection(r"pythonsqlite.db")
+    conn = create_connection(r"pythonsqlite.db")
+    build_database(conn)
+    conn.close()
+    print("Created database")
