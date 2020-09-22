f91da1ad5699f56fc7357c8d5001175110367de3
matttm <matttmaloney@gmail.com>
2020-09-22 18:36:53 -0400

    creates a db

--- records-backend/gendb.py
+++ records-backend/gendb.py
@@ -1,8 +1,18 @@
 import sqlite3
+import sys
+import argparse
+from faker import Faker
 from sqlite3 import Error
 """
 Generate a database of records
 """
+
+INSERT_SQL = "INSERT INTO person (ssn, fname, lname, zip) VALUES (?, ?, ?, ?)"
+
+def parse_arguments():
+    short_options = "n:flsz"
+    long_options = []
+    pass
 
 def create_connection(db_file):
     """ create a database connection to a SQLite database """
@@ -26,20 +36,32 @@ def build_database(conn):
     table = """
     CREATE TABLE person (
         id integer PRIMARY KEY,
-        ssn text not null,
-        Fname text NOT NULL,
-        lname text NOT NULL,
-        zip text NOT NULL
+        ssn VARCHAR(9) not null,
+        Fname VARCHAR(45) NOT NULL,
+        lname VARCHAR(45) NOT NULL,
+        zip VARCHAR(5) NOT NULL
     )"""
     cur.execute(table)
-    insert_sql = "INSERT INTO person (ssn, fname, lname, zip) VALUES (?, ?, ?, ?)"
-    cur = conn.cursor()
-    cur.execute(insert_sql, ('1892029', 'afname', 'alname', '19029'))
-    conn.commit()
 
+
+def add_person(cur, ssn, fname, lname, zip):
+    cur.execute(INSERT_SQL, (ssn, fname, lname, zip))
+
+def add_dummy_persons(number_of_persons, faker, conn):
+    cur = conn.cursor()
+    for i in range(number_of_persons):
+        fname, lname = faker.name().split()[0:2]
+        ssn = faker.ssn()
+        zip = faker.postcode()
+        add_person(cur, ssn, fname, lname, zip)
 
 if __name__ == '__main__':
+    number_of_persons = 100
+    Faker.seed(0)
+    faker = Faker()
     conn = create_connection(r"pythonsqlite.db")
     build_database(conn)
+    add_dummy_persons(number_of_persons, faker, conn)
+    conn.commit()
     conn.close()
     print("Created database")
