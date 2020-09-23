18c49b060fac8585ac3b39c7be8265c824d6af31
matttm <matttmaloney@gmail.com>
2020-09-23 16:31:51 -0400

    finished db generation

--- records-backend/gendb.py
+++ records-backend/gendb.py
@@ -1,37 +1,43 @@
 import sqlite3
-import sys
 import argparse
+import sys
+import re
 from faker import Faker
 from sqlite3 import Error
 """
-Generate a database of records
+Generate a database of records with the command line arguments as an entry
 """
 
 INSERT_SQL = "INSERT INTO person (ssn, fname, lname, zip) VALUES (?, ?, ?, ?)"
 
 def parse_arguments():
-    short_options = "n:flsz"
-    long_options = []
-    pass
+    """
+    Parse arguments
+    """
+    parser = argparse.ArgumentParser()
+    parser.add_argument("number_of_records", help="number of people", type=int)
+    parser.add_argument("firstname",         help="first name off entry")
+    parser.add_argument("lastname",          help="last name off entry")
+    parser.add_argument("ssn",               help="hyphenated ssn of entry")
+    parser.add_argument("zip",               help="zip code off entry")
+    args = parser.parse_args()
+    return args
 
 def create_connection(db_file):
     """ create a database connection to a SQLite database """
     conn = None
     try:
         conn = sqlite3.connect(db_file)
-        print(sqlite3.version)
         return conn
     except Error as e:
         print(e)
         exit(-1)
 
-def build_database(conn):
+def build_database(cur):
     """
     Build 'persons' table in db
-    :param ocnn: a sqlite3 connection that is open
-    :return:
+    :param cur: a database connection cursor
     """
-    cur = conn.cursor()
     # TODO: change table name to 'persons'
     table = """
     CREATE TABLE person (
@@ -45,10 +51,23 @@ def build_database(conn):
 
 
 def add_person(cur, ssn, fname, lname, zip):
+    """
+    A a person record to persons table
+    :param cur: a database connection's cursor
+    :param ssn: a ssn of entry
+    :param fname: first name of entry
+    :param lname: last name of entry
+    :param zip: zip code of entry
+    """
     cur.execute(INSERT_SQL, (ssn, fname, lname, zip))
 
-def add_dummy_persons(number_of_persons, faker, conn):
-    cur = conn.cursor()
+def add_dummy_persons(number_of_persons, faker, cur):
+    """
+    Adds a given number of person records to the persons table
+    :param number_of_persons: number of records to add
+    :param faker: an object to generate fake data
+    :param cur: a database connection cursor
+    """
     for i in range(number_of_persons):
         fname, lname = faker.name().split()[0:2]
         ssn = faker.ssn()
@@ -56,12 +75,29 @@ def add_dummy_persons(number_of_persons, faker, conn):
         add_person(cur, ssn, fname, lname, zip)
 
 if __name__ == '__main__':
-    number_of_persons = 100
+    args  = parse_arguments()
+    number_of_records = args.number_of_records
+    fname = args.firstname
+    lname = args.lastname
+    ssn   = args.ssn
+    zip   = args.zip
+    if bool(re.match("^(\d{3}-\d{2}-\d{4})", ssn)) is False:
+        print("Social Security Number does not follow the pattern: XXX-XX-XXXX")
+        sys.exit(0)
+
+    # seed generator of fake data
     Faker.seed(0)
     faker = Faker()
-    conn = create_connection(r"pythonsqlite.db")
-    build_database(conn)
-    add_dummy_persons(number_of_persons, faker, conn)
-    conn.commit()
-    conn.close()
-    print("Created database")
+    try:
+        conn = create_connection(r"pythonsqlite.db")
+        cur = conn.cursor()
+        build_database(cur)
+        add_dummy_persons(number_of_records, faker, cur)
+        # add non random entry
+        add_person(cur, ssn, fname, lname, zip)
+        conn.commit()
+        conn.close()
+        print("Created database")
+    except sqlite3.OperationalError:
+        print("Person table already exists)")
+        print("Shutting down...")
