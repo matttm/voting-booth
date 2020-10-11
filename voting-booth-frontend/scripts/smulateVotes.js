f916c896e6656b2661c84c8ad1e67dcca6686e15
matttm <matttmaloney@gmail.com>
2020-10-11 16:10:56 -0400

    integrating sqlite into simulation script

--- voting-booth-frontend/scripts/smulateVotes.js
+++ voting-booth-frontend/scripts/smulateVotes.js
@@ -1,5 +1,12 @@
 import request from 'superagent';
-import { promisify } from 'util';
+import sqlite from 'sqlite3';
+
+let db = sqlite.verbose().Database('../records-backend/records.db', err => {
+  if (err) {
+    console.log(`Database connection error encountered: ${err}`);
+  }
+  console.log('Database connection established')
+});
 /**
  * This script is designed to simulate votes, where each vote will be
  * by a person in the records.db and for a random candidate from candidate.json
@@ -20,29 +27,41 @@ const voters = [
 const candidates = ['Joe Biden'];
 
 /**
- * We use `length - 1` here because we do not want to submit a vote
- * as the last voter entry, which will be the person running the simulation
+ * Must wrap this in an async IIFE, so I can use await
  */
-for (let i = 0; i < voters.length - 1; i++) {
-  // TODO: get voter credenials
-  const credentials = voters[i];
-  let response = await request
-    .post(`${BACKEND_URL}/api/login`)
-    .send(credentials);
-  if (response.status !== 200) {
-    throw new Error(`Script encountered an error`);
+(async () => {
+  /**
+   * We use `length - 1` here because we do not want to submit a vote
+   * as the last voter entry, which will be the person running the simulation
+   */
+  for (let i = 0; i < voters.length - 1; i++) {
+    // TODO: get voter credenials
+    const credentials = voters[i];
+    let response = await request
+      .post(`${BACKEND_URL}/api/login`)
+      .send(credentials);
+    if (response.status !== 200) {
+      throw new Error(`Script encountered an error`);
+    }
+    const bearerToken = `Bearer ${response.body.bearerToken}`;
+    // TODO: make index random
+    const candidate = candidates[0];
+    response = await request
+      .post(`${BACKEND_URL}/api/votes`)
+      .set('Authorization', bearerToken)
+      .send({ candidate });
+    if (response.status !== 200) {
+      throw new Error(`Script encountered an error`);
+    }
+    console.log(`Sent simulated vote`);
+    // wait for a minute
+    await sleep(1000 * 600);
   }
-  const bearerToken = `Bearer ${response.body.bearerToken}`;
-  // TODO: make index random
-  const candidate = candidates[0];
-  response = await request
-    .post(`${BACKEND_URL}/api/votes`)
-    .set('Authorization', bearerToken)
-    .send({ candidate });
-  if (response.status !== 200) {
-    throw new Error(`Script encountered an error`);
+})();
+
+db.close(err => {
+  if (err) {
+    console.log(`Database connection error encountered: ${err}`);
   }
-  console.log(`Sent simulated vote`);
-  // wait for a minute
-  sleep(1000 * 600);
-}
+  console.log('Database connection closed')
+});
