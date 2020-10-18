d36e9edfd4aa9dae036b1e668956f7d836c4f457
matttm <matttmaloney@gmail.com>
2020-10-18 11:44:22 -0400

    script is working again

--- voting-booth-frontend/scripts/simulateVotes.js
+++ voting-booth-frontend/scripts/simulateVotes.js
@@ -76,7 +76,7 @@ db.each(sql, (err, row) => {
       if (authResponse.status !== 200) {
         throw new Error(`Script encountered an error`);
       }
-      console.log(`Token ${bearerToken}`);
+      const bearerToken =`Bearer ${authResponse.body?.idToken}`;
       // TODO: make index random
       const candidate = candidates[0];
       request
