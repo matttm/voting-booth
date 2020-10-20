1e25640475883c1da3eb719b0b78ef0e115f423f
matttm <matttmaloney@gmail.com>
2020-10-20 14:46:03 -0400

    finished test for mapping

--- voting-booth-frontend/src/app/components/result/result.component.spec.ts
+++ voting-booth-frontend/src/app/components/result/result.component.spec.ts
@@ -1,8 +1,8 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { ResultComponent } from './result.component';
 import {MatTableModule} from '@angular/material';
-import {Result, ResultsResponse} from "../../types";
+import {Result, ResultsResponse} from '../../types';
+import {HttpClientTestingModule} from '@angular/common/http/testing';
 
 describe('ResultComponent', () => {
   let component: ResultComponent;
@@ -10,7 +10,10 @@ describe('ResultComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports: [MatTableModule],
+      imports: [
+        MatTableModule,
+        HttpClientTestingModule
+      ],
       declarations: [ ResultComponent ]
     })
     .compileComponents();
@@ -32,18 +35,31 @@ describe('ResultComponent', () => {
   });
 
   it('should map a ResultResponse to a Result[]', () => {
+    const name1 = 'Joe Biden';
+    const votes1 = 100;
+    const name2 = 'Timmy Toetoe';
+    const votes2 = 50;
     const body: ResultsResponse = {
       results: [
         [
-          'Joe Biden',
-          100
+          name1,
+          votes1
+        ],
+        [
+          name2,
+          votes2
         ]
       ],
       success: true
     };
     const mapped: Result[] = ResultComponent.translateResultsFromWire(body);
-    expect(mapped.length).toBe(1);
-    const el = mapped[0];
+    expect(mapped.length).toBe(2);
+    let el = mapped[0];
+    expect(el.name).toBe(name1);
+    expect(el.votes).toBe(votes1);
+    el = mapped[1];
+    expect(el.name).toBe(name2);
+    expect(el.votes).toBe(votes2);
 
     return null;
   });
