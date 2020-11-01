f6d3c19aa7f3cc2102990e7bad1843527fb83357
matttm <matttmaloney@gmail.com>
2020-11-01 12:57:35 -0500

    made booth button disabled when form is invalid

--- voting-booth-frontend/src/app/components/booth/booth.component.ts
+++ voting-booth-frontend/src/app/components/booth/booth.component.ts
@@ -38,31 +38,24 @@ export class BoothComponent {
   handleSubmit() {
     this.isVoting = true;
     const selected = this.selected;
-    // check whether somebody is actually selected
-    if (selected) {
-      console.log('Voting for: ' + selected);
-      this.votingService.vote(selected)
-        .then(() => {
-          this.isVoting = false;
-          this.snackbar.open('Vote Submitted', null, {duration: 5000});
-        })
-        .catch(err => {
-          // TODO: factor to an error handler
-          let message = '';
-          const status = err.status;
-          if (status === 503) {
-            message = 'We are experiencing difficulties';
-          } else if (status === 401) {
-            message = 'Login before voting';
-          }
-          this.snackbar
-            .open(message, null, {duration: 6000})
-            .afterDismissed().subscribe();
-        });
-    } else {
-      this.isVoting = false;
-      this.snackbar
-        .open('Select a candidate before submitting', null, {duration: 5000});
-    }
+    console.log('Voting for: ' + selected);
+    this.votingService.vote(selected)
+      .then(() => {
+        this.isVoting = false;
+        this.snackbar.open('Vote Submitted', null, {duration: 5000});
+      })
+      .catch(err => {
+        // TODO: factor to an error handler
+        let message = '';
+        const status = err.status;
+        if (status === 503) {
+          message = 'We are experiencing difficulties';
+        } else if (status === 401) {
+          message = 'Login before voting';
+        }
+        this.snackbar
+          .open(message, null, {duration: 6000})
+          .afterDismissed().subscribe();
+      });
   }
 }
