b4cfc5c337398e90849a711bd226d962f6a4b478
matttm <matttmaloney@gmail.com>
2020-11-07 10:48:04 -0500

    cleanup

--- voting-booth-frontend/src/app/components/submission/submission.component.ts
+++ voting-booth-frontend/src/app/components/submission/submission.component.ts
@@ -1,19 +1,15 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, Input} from '@angular/core';
 
 @Component({
   selector: 'app-submission',
   templateUrl: './submission.component.html',
   styleUrls: ['./submission.component.css']
 })
-export class SubmissionComponent implements OnInit {
+export class SubmissionComponent {
   @Input()
   conditionVar: boolean;
   @Input()
   submissionText: string;
 
   constructor() { }
-
-  ngOnInit() {
-  }
-
 }
