fbfdbfb335d7a80da5256dcde2b5cb976cb21789
matttm <matttmaloney@gmail.com>
2020-12-20 14:51:36 -0500

    added error handling in failsafe

--- voting-booth-frontend/src/app/components/failsafe/failsafe.component.ts
+++ voting-booth-frontend/src/app/components/failsafe/failsafe.component.ts
@@ -1,6 +1,6 @@
 import {Component, Inject} from '@angular/core';
 import {VotingService} from '../../services/voting/voting.service';
-import {MAT_DIALOG_DATA} from "@angular/material";
+import {MAT_DIALOG_DATA, MatSnackBar} from "@angular/material";
 
 @Component({
   selector: 'app-failsafe',
@@ -14,7 +14,8 @@ export class FailsafeComponent {
 
   constructor(
     private votingService: VotingService,
-    @Inject(MAT_DIALOG_DATA) public data: any
+    @Inject(MAT_DIALOG_DATA) public data: any,
+    private snackbar: MatSnackBar
   ) {
     this.isAskingEmail = false;
     this.email = null;
@@ -25,7 +26,11 @@ export class FailsafeComponent {
    */
   onEmailSubmit() {
     // TODO:do anything with this promise?
-    this.votingService.voteFailsafe(this.data.candidate, this.email);
+    this.votingService.voteFailsafe(this.data.candidate, this.email)
+      .then(res => this.snackbar
+        .open('Failsafe service succeeded', null, {duration: 6000}))
+      .catch(err => this.snackbar
+        .open('Failsafe service failed', null, {duration: 6000}));
     console.log(`Failsafe vote filed ${this.data.candidate} ${this.email}`);
   }
 
