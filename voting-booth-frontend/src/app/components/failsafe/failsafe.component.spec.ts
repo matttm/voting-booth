fe30beec21aee6f8b2da21a01b0ee39137a3b8a7
matttm <matttmaloney@gmail.com>
2021-02-19 16:25:45 -0500

    fixed unit test

--- voting-booth-frontend/src/app/components/failsafe/failsafe.component.spec.ts
+++ voting-booth-frontend/src/app/components/failsafe/failsafe.component.spec.ts
@@ -1,6 +1,11 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import {MAT_DIALOG_DATA, MatDialogRef} from '@angular/material';
 
 import { FailsafeComponent } from './failsafe.component';
+import {MatDialogModule, MatFormFieldModule, MatSnackBarModule} from '@angular/material';
+import {FormsModule} from '@angular/forms';
+import {HttpClient} from '@angular/common/http';
+import {HttpClientTestingModule} from '@angular/common/http/testing';
 
 describe('FailsafeComponent', () => {
   let component: FailsafeComponent;
@@ -8,7 +13,19 @@ describe('FailsafeComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ FailsafeComponent ]
+      imports: [
+        FormsModule,
+        MatDialogModule,
+        MatSnackBarModule,
+        HttpClientTestingModule,
+        MatFormFieldModule
+      ],
+      declarations: [ FailsafeComponent ],
+      providers: [
+        { provide: MAT_DIALOG_DATA, useValue: {} },
+        { provide: MatDialogRef, useValue: {} },
+        { provide: HttpClient, useValue: {}}
+      ]
     })
     .compileComponents();
   }));
