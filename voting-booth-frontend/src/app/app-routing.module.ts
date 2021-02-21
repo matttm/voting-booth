ec32d5fe629f37850c1b6396debdd879466c662d
matttm <matttmaloney@gmail.com>
2021-02-21 12:01:11 -0500

    added auth-guard

--- voting-booth-frontend/src/app/app-routing.module.ts
+++ voting-booth-frontend/src/app/app-routing.module.ts
@@ -4,11 +4,12 @@ import {LoginComponent} from './components/login/login.component';
 import {SplashComponent} from './components/splash/splash.component';
 import {BoothComponent} from './components/booth/booth.component';
 import {ResultComponent} from './components/result/result.component';
+import {AuthGuard} from './services/guards/auth/auth.guard';
 
 const routes: Routes = [
   { path: '', component: SplashComponent },
   { path: 'login', component: LoginComponent },
-  { path: 'booth', component: BoothComponent },
+  { path: 'booth', component: BoothComponent, canActivate: [AuthGuard] },
   { path: 'results', component: ResultComponent }
 ];
 
---
+++ voting-booth-frontend/src/app/services/guards/auth/auth.guard.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import {ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree} from '@angular/router';
+import { Observable } from 'rxjs';
+
+/**
+ * This AuthGuard will check localStorage to check for a JWT,
+ * ensuring whether a user has logged in
+ */
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate {
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    return !!localStorage.getItem('id_token');
+  }
+}
