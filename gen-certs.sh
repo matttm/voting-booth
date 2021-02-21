f26faff14f93f0299dcbb7c8291f5dd6d05d287b
matttm <matttmaloney@gmail.com>
2021-02-21 12:58:28 -0500

    moving cert pw to env

--- gen-certs.sh
+++ gen-certs.sh
@@ -2,10 +2,10 @@
 openssl req -new -x509 -newkey rsa:2048 -sha256 -nodes -keyout certificate.key -days 3560 -out certificate.crt -config certificate.cnf
 
 # convert crt to p12
-openssl pkcs12 -export -in certificate.crt -inkey certificate.key -name localhost -out certificate.p12
+echo -e "${VB_CERT_PASSWORD}" | openssl pkcs12 -export -in certificate.crt -inkey certificate.key -name localhost -out certificate.p12
 
 # import p12 to jks store
-keytool -importkeystore -deststorepass password -destkeystore keystore.jks -srckeystore certificate.p12 -srcstoretype PKCS12
+echo -e "${VB_CERT_PASSWORD}" | keytool -importkeystore -deststorepass password -destkeystore keystore.jks -srckeystore certificate.p12 -srcstoretype PKCS12
 
 # moving store to record's resources
 mv keystore.jks records-backend/src/main/resources
