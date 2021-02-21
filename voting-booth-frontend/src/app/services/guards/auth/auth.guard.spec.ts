17543a12381e8fae2b1ec6210a2987081c58a3e1
matttm <matttmaloney@gmail.com>
2021-02-21 12:17:30 -0500

    added routing to auth-guard

--- voting-booth-frontend/src/app/services/guards/auth/auth.guard.spec.ts
+++ voting-booth-frontend/src/app/services/guards/auth/auth.guard.spec.ts
@@ -1,10 +1,14 @@
 import {inject, TestBed} from '@angular/core/testing';
 
 import {AuthGuard} from './auth.guard';
+import {RouterTestingModule} from '@angular/router/testing';
 
 describe('AuthGuard', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
+      imports: [
+        RouterTestingModule
+      ],
       providers: [AuthGuard]
     });
   });
--- voting-booth-frontend/src/app/services/guards/auth/auth.guard.ts
+++ voting-booth-frontend/src/app/services/guards/auth/auth.guard.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import {ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree} from '@angular/router';
+import {ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree} from '@angular/router';
 import { Observable } from 'rxjs';
 
 /**
@@ -10,10 +10,19 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate {
+  constructor(private router: Router) {}
+
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot
   ): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return !!localStorage.getItem('id_token');
+    const authorized = localStorage.getItem('id_token');
+    if (authorized) {
+      return true;
+    } else {
+      this.router.navigateByUrl('login')
+        .then(() => console.log('Redirecting unauthorized user to loggin page'));
+      return false;
+    }
   }
 }
